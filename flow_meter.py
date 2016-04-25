9ad8457245b11e0cf1fda6467b84b93e85ec7e23
joram <john@oram.ca>
2016-04-25 03:17:23 +0000

    flowmeter class added

--- flow_meter.py
+++ flow_meter.py
@@ -2,28 +2,45 @@
 
 import RPi.GPIO as GPIO
 from time import sleep
+from datetime import timedelta, datetime
 
-# The script as below using BCM GPIO 00..nn numbers
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(19, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(13, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+class FlowMeter(object):
 
-count_13 = 0
-count_19 = 0
-def my_callback_19(channel):  
-	global count_19
-	count_19 += 1
-	print "pin_19:%d, pin_13:%d" % (count_19, count_13)
+	def __init__(self, pin, ml_per_pulse=49, max_history_length=100, max_history_delta=timedelta(minutes=1)):
+		self.pin = pin
+		self.max_history_length = max_history_length
+		self.max_history_delta = max_history_delta
+		self.ml_per_pulse = ml_per_pulse
+		self.pulse_history = []
 
-def my_callback_13(channel):  
-	global count_13
-	count_13 += 1
-	print "pin_19:%d, pin_13:%d" % (count_19, count_13)
+		GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+		GPIO.add_event_detect(pin, GPIO.FALLING, callback=self._pulse, bouncetime=30)
 
-GPIO.add_event_detect(19, GPIO.FALLING, callback=my_callback_19, bouncetime=300)
-GPIO.add_event_detect(13, GPIO.FALLING, callback=my_callback_13, bouncetime=300)
+	def _pulse(self, channel):
+		print channel
+		self.pulse_history.append(datetime.now())
+		self._trim_history()
 
-# Set relay pins as output
-while True:
-	sleep(10)
+	def _trim_history(self):
+		if len(self.pulse_history) > self.max_history_length:
+			self.pulse_history = self.pulse_history[self.max_history_length:]
+
+		while len(self.pulse_history) > 0:
+			delta = datetime.now() - self.pulse_history[0]
+			if delta < self.max_history_delta:
+				break
+			del self.pulse_history[0]
+
+	def flow(self):
+		self._trim_history()
+		return self.ml_per_pulse * len(self.pulse_history)
+
+if __name__ == "__main__":
+	GPIO.setmode(GPIO.BCM)
+	pins = [13, 19]
+	flow_meters = [FlowMeter(pin) for pin in pins]
+	while True:
+		for fm in flow_meters:
+			print "FlowMeter(%d) at %sml/min" % (fm.pin, fm.flow())
+		sleep(3)
 
